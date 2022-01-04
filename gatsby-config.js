e92cd23078529cc6fe9cf847b55324751e6d4eeb
James Armer <jamesarmer2@gmail.com>
2022-01-04 21:28:57 +0000

    Initial config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,14 @@
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
-    title: "james-portfolio",
+    title: "James Portfolio",
+    description: "A personal porfolio website built using Gtasby JS",
+    author: "James Armer",
   },
-  plugins: [],
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-sass`,
+  ],
 };
